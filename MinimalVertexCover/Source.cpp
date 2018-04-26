fa6b988661ff74dda4ec00a67897d6018a0dc1e7
philippe debie <philippe.mmd@gmail.com>
2018-04-26 14:28:15 +0200

    Added fully connected filter

--- MinimalVertexCover/Source.cpp
+++ MinimalVertexCover/Source.cpp
@@ -14,21 +14,7 @@ struct Graph
 
 struct VertexCover
 {
-	int amount;
 	std::vector<bool> nodeIsCover;
-
-	int getCoverSize()
-	{
-		int sum = 0;
-		for (auto nc : nodeIsCover)
-		{
-			if (nc)
-			{
-				sum++;
-			}
-		}
-		return sum;
-	}
 };
 
 Graph generateGraph(int nodes, int edges)
@@ -62,48 +48,92 @@ Graph generateGraph(int nodes, int edges)
 
 bool isVertexCover(Graph g, VertexCover vc)
 {
-	std::vector<bool> nodeCovered;
 	for (int i = 0; i < g.amountNodes; i++)
 	{
-		nodeCovered.push_back(false);
-	}
-	for (int i = 0; i < g.amountNodes; i++)
-	{
-		if (vc.nodeIsCover[i])
+		if (!vc.nodeIsCover[i])
 		{
-			nodeCovered[i] = true;
+			bool found = false;
 
 			for (auto e : g.edges)
 			{
 				if (i == e.first)
 				{
-					nodeCovered[e.second] = true;
+					if (vc.nodeIsCover[e.second])
+					{
+						found = true;
+					}
 				}
 				if (i == e.second)
 				{
-					nodeCovered[e.first] = true;
+					if (vc.nodeIsCover[e.first])
+					{
+						found = true;
+					}
+				}
+			}
+
+			if (!found)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool isFullyConnected(const Graph& g)
+{
+	std::vector<int> explored;
+
+	std::vector<int> toExplore;
+	toExplore.push_back(0);
+
+	while (toExplore.size() > 0)
+	{
+		int i = toExplore.back();
+		toExplore.pop_back();
+		explored.push_back(i);
+
+		for (auto e : g.edges)
+		{
+			if (i == e.first)
+			{
+				int other = e.second;
+				if (std::find(explored.begin(), explored.end(), other) == explored.end())
+				{
+					if (std::find(toExplore.begin(), toExplore.end(), other) == toExplore.end())
+					{
+						toExplore.push_back(other);
+					}
+				}
+			}
+			else if (i == e.second)
+			{
+				int other = e.first;
+				if (std::find(explored.begin(), explored.end(), other) == explored.end())
+				{
+					if (std::find(toExplore.begin(), toExplore.end(), other) == toExplore.end())
+					{
+						toExplore.push_back(other);
+					}
 				}
 			}
 		}
 	}
-	bool allCovered = true;
-	for (int i = 0; i < g.amountNodes; i++)
-	{
-		if (nodeCovered[i] == false)
-		{
-			allCovered = false;
-		}
-	}
-	return allCovered;
+
+	return explored.size() == g.amountNodes;
 }
 
-void findAllCovers(Graph& g, VertexCover& vc, std::vector<VertexCover>& allVcs, int& minimum)
+void findAllCovers(Graph& g, VertexCover& vc, std::vector<VertexCover>& allVcs, int& minimum, int size=0)
 {
+	if (size > minimum)
+	{
+		return;
+	}
 	if (vc.nodeIsCover.size() == g.amountNodes)
 	{
 		if (isVertexCover(g, vc))
 		{
-			int size = vc.getCoverSize();
 			if (size == minimum)
 			{
 				allVcs.push_back(vc);
@@ -119,12 +149,12 @@ void findAllCovers(Graph& g, VertexCover& vc, std::vector<VertexCover>& allVcs, 
 	else
 	{
 		auto vc1 = vc;
-		vc1.nodeIsCover.push_back(true);
-		findAllCovers(g, vc1, allVcs, minimum);
+		vc1.nodeIsCover.push_back(false);
+		findAllCovers(g, vc1, allVcs, minimum, size);
 
 		auto vc2 = vc;
-		vc2.nodeIsCover.push_back(false);
-		findAllCovers(g, vc2, allVcs, minimum);
+		vc2.nodeIsCover.push_back(true);
+		findAllCovers(g, vc2, allVcs, minimum, size + 1);
 	}
 }
 
@@ -145,9 +175,14 @@ int main()
 		return 0;
 	}
 
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 1000000; i++)
 	{
 		auto g = generateGraph(nodes, edges);
+
+		while (!isFullyConnected(g))
+		{
+			g = generateGraph(nodes, edges);
+		}
 
 		f << g.amountNodes;
 		for (int j = 0; j < g.edges.size(); j++)
@@ -156,7 +191,6 @@ int main()
 		}
 
 		VertexCover prototype;
-		prototype.amount = nodes;
 
 		int minimum = nodes;
 		std::vector<VertexCover> allVcs;
@@ -187,7 +221,17 @@ int main()
 			maxMin = allVcs.size();
 		}
 
-		std::cout << "Minimum cover: " << minimum << std::endl;
+		if (i % 100 == 0 && i > 0)
+		{
+			std::cout << ".";
+		}
+		if (i % 10000 == 0 && i > 0)
+		{
+			std::cout << i << std::endl;
+		}
+
+
+		//std::cout << "Minimum cover: " << minimum << std::endl;
 
 		//std::cout << "Covers found:  " << allVcs.size() << std::endl << std::endl;
 	}
